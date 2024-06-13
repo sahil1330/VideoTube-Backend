ef1ca09e7bb86c421e3bc492872b133ab810a11b
sahil1330 <sahilmane025@gmail.com>
2024-06-13 19:33:10 +0530

    Update getCurrentUser response format to use ApiResponse class

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -254,7 +254,7 @@ const changeCurrentPassword = asyncHandler(async (req, res) => {
 const getCurrentUser = asyncHandler(async (req, res) => {
   return res
     .status(200)
-    .json(200, req.user, "Current User fetched Successfully.");
+    .json(new ApiResponse(200, req.user, "Current User fetched Successfully."));
 });
 
 const updateAccountDetails = asyncHandler(async (req, res) => {
@@ -263,7 +263,7 @@ const updateAccountDetails = asyncHandler(async (req, res) => {
     throw new ApiError(400, "All fields are required.");
   }
 
-  const user = User.findByIdAndUpdate(
+  const user = await User.findByIdAndUpdate(
     req.user?._id,
     {
       $set: {
@@ -286,6 +286,8 @@ const updateUserAvatar = asyncHandler(async (req, res) => {
   }
 
   const avatar = await uploadOnCloudinary(avatarLocalPath);
+
+  
 
   if (!avatar.url) {
     throw new ApiError(400, "Error while uploading on avatar.");
