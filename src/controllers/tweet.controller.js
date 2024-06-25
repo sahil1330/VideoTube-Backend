6d94159993bedb35430ae754780f6fe87f4bc099
sahil1330 <sahilmane025@gmail.com>
2024-06-25 08:44:21 +0530

    chore: Update tweet controller and routes to handle tweet image uploads and error handling improvements

--- src/controllers/tweet.controller.js
+++ src/controllers/tweet.controller.js
@@ -11,14 +11,16 @@ const createTweet = asyncHandler(async (req, res) => {
   if (!content) {
     throw new ApiError(400, "Content is required");
   }
+
   let tweetImageLocalPath;
-  if (
-    req.files &&
-    Array.isArray(req.files.tweetImage) &&
-    req.files.tweetImage.length > 0
-  ) {
-    tweetImageLocalPath = req.files.tweetImage[0].path;
+  if (req.file) {
+    try {
+      tweetImageLocalPath = req.file?.path;
+    } catch (error) {
+      throw new ApiError(400, "Error uploading tweet image");
+    }
   }
+
   let tweetImage;
   if (tweetImageLocalPath) {
     try {
@@ -30,7 +32,7 @@ const createTweet = asyncHandler(async (req, res) => {
       );
     }
   }
-  
+
   const tweet = await Tweet.create({
     content,
     owner: req.user?._id,
@@ -38,7 +40,7 @@ const createTweet = asyncHandler(async (req, res) => {
   });
 
   if (!tweet) {
-    return new ApiError(400, "Error creating tweet.");
+    throw new ApiError(400, "Error creating tweet.");
   }
 
   const populatedTweet = await Tweet.findById(tweet._id).populate(
@@ -51,4 +53,85 @@ const createTweet = asyncHandler(async (req, res) => {
     .json(new ApiResponse(201, populatedTweet, "Tweet created successfully."));
 });
 
-export { createTweet };
+const getUserTweets = asyncHandler(async (req, res) => {
+  const { userId } = req.params;
+  if (!isValidObjectId(userId)) {
+    throw new ApiError(400, "Invalid User ID");
+  }
+  const tweets = await Tweet.find({
+    owner: userId,
+  })
+    .select("-owner")
+    .sort({ createdAt: -1 });
+
+  if (!tweets) {
+    throw new ApiError(404, "No tweets found");
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, tweets, "User tweets fetched successfully."));
+});
+
+const updateTweet = asyncHandler(async (req, res) => {
+  const { tweetId } = req.params;
+  const { content } = req.body;
+  const { tweetImage } = req.files.tweetImage[0].path;
+
+  if (!isValidObjectId(tweetId)) {
+    throw new ApiError(400, "Invalid Tweet ID");
+  }
+
+  const tweet = await findByIdAndUpdate(
+    tweetId,
+    {
+      content,
+      contentImage: tweetImage,
+    },
+    { new: true }
+  ).populate("owner", "-password -refreshToken");
+
+  if (!tweet) {
+    throw new ApiError(400, "Error updating tweet.");
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, tweet, "Tweet updated successfully"));
+});
+
+const deleteTweet = asyncHandler(async (req, res) => {
+  const { tweetId } = req.params;
+
+  if (!isValidObjectId(tweetId)) {
+    throw new ApiError(400, "Invalid Tweet ID");
+  }
+
+  const tweet = await Tweet.findById(tweetId);
+
+  if (!tweet) {
+    throw new ApiError(404, "Tweet not found");
+  }
+
+  const tweetImage = tweet.contentImage;
+
+  try {
+    await Tweet.findByIdAndDelete(tweetId);
+  } catch (error) {
+    throw new ApiError(400, "Error deleting tweet.");
+  }
+
+  if (tweetImage) {
+    try {
+      await deleteFromCloudinary(tweetImage);
+    } catch (error) {
+      throw new ApiError(400, "Error deleting tweet image.");
+    }
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, {}, "Tweet deleted successfully"));
+});
+
+export { createTweet, getUserTweets, updateTweet, deleteTweet };
--- src/routes/tweet.routes.js
+++ src/routes/tweet.routes.js
@@ -1,20 +1,23 @@
 import Router from "express";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
-import { createTweet } from "../controllers/tweet.controller.js";
+import {
+  createTweet,
+  deleteTweet,
+  getUserTweets,
+  updateTweet,
+} from "../controllers/tweet.controller.js";
 
 const router = Router();
 
 router.use(verifyJWT);
 
-router.route("/").post(
-  upload.fields([
-    {
-      name: "tweetImage",
-      maxCount: 5,
-    },
-  ]),
-  createTweet
-);
+router.route("/").post(upload.single("tweetImage"), createTweet);
+
+router.route("/user/:userId").get(getUserTweets);
+router
+  .route("/:tweetId")
+  .patch(upload.single("tweetImage"), updateTweet)
+  .delete(deleteTweet);
 
 export default router;
