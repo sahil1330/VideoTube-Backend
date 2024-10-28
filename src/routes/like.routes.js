3e563dfb249cf73c17619df4caeb6a4b7833cc39
sahil1330 <sahilmane025@gmail.com>
2024-10-28 19:04:23 +0530

    Refactor like routes to include toggle comment, video and tweet like functionality

--- src/routes/like.routes.js
+++ src/routes/like.routes.js
@@ -2,15 +2,16 @@ import { Router } from "express";
 import {
   getLikedVideos,
   toggleTweetLike,
+  toggleCommentLike,
   toggleVideoLike,
 } from "../controllers/like.controller.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 router.use(verifyJWT);
-router.route("/:videoId").post(toggleVideoLike);
-router.route("/:commentId").post(toggleVideoLike);
-router.route("/:tweetId").post(toggleTweetLike);
+router.route("/toggle-video-like/:videoId").post(toggleVideoLike);
+router.route("/toggle-comment-like/:commentId").post(toggleCommentLike);
+router.route("/toggle-tweet-like/:tweetId").post(toggleTweetLike);
 router.route("/").get(getLikedVideos);
 
 export default router;
