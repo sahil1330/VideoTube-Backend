c3e959ecec7fd6bf68661714102a3fa04103b28f
sahil1330 <sahilmane025@gmail.com>
2024-11-21 00:10:07 +0530

    Add dashboard routes and controller for channel statistics and videos

--- src/app.js
+++ src/app.js
@@ -25,6 +25,7 @@ import commentRouter from "./routes/comment.routes.js";
 import playlistRouter from "./routes/playlist.routes.js";
 import likeRouter from "./routes/like.routes.js";
 import subscriptionRouter from "./routes/subscription.routes.js";
+import dashboardRouter from "./routes/dashboard.routes.js";
 
 // routes declaration
 // http://localhost:8000/api/v1/users/register
@@ -37,5 +38,6 @@ app.use("/api/v1/comments", commentRouter);
 app.use("/api/v1/playlists", playlistRouter);
 app.use("/api/v1/likes", likeRouter);
 app.use("/api/v1/subscriptions", subscriptionRouter);
+app.use("/api/v1/dashboard", dashboardRouter);
 
 export { app };
--- src/controllers/dashboard.controller.js
+++ src/controllers/dashboard.controller.js
@@ -1,13 +1,115 @@
-import mongoose from "mongoose";
+import mongoose, { isValidObjectId } from "mongoose";
 import { Subscription } from "../models/subscription.model.js";
 import { User } from "../models/user.model.js";
 import { Video } from "../models/video.model.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { Like } from "../models/like.model.js";
 
 const getChannelStats = asyncHandler(async (req, res) => {
   // TODO: Get the channel stats like total video views, total subscribers, total videos, total likes etc.
 
-  // Get total video views
+  const { channelId } = req.params;
+
+  // Get total video views of a channel
+  const totalVideoViews = await Video.aggregate([
+    {
+      $match: {
+        owner: new mongoose.Types.ObjectId("6668adfecd3df591e0c55aa2"),
+      },
+    },
+    {
+      $group: {
+        _id: null,
+        viewsCount: {
+          $sum: "$views",
+        },
+      },
+    },
+  ]);
+
+  if (!totalVideoViews) {
+    throw new ApiError(400, "Error fetching total video views.");
+  }
+
+  // Get total subscribers of a channel
+  const totalSubscribers = await Subscription.countDocuments({
+    channel: channelId,
+  }).catch((error) => {
+    throw new ApiError(400, "Error fetching total subscribers.");
+  });
+
+  // Get total videos of a channel
+  const totalVideos = await Video.countDocuments({
+    owner: channelId,
+  }).catch((error) => {
+    throw new ApiError(400, "Error fetching total videos.");
+  });
+
+  // Get total likes on videos of a channel
+  const totalLikes = await Video.aggregate([
+    {
+      $match: {
+        owner: new mongoose.Types.ObjectId("6668adfecd3df591e0c55aa2"),
+      },
+    },
+    {
+      $lookup: {
+        from: "likes",
+        localField: "_id",
+        foreignField: "video",
+        as: "likes",
+      },
+    },
+    {
+      $unwind: "$likes",
+    },
+    {
+      $group: {
+        _id: null,
+        totalNumberOfLikes: {
+          $sum: 1,
+        },
+      },
+    },
+  ]);
+
+  if (!totalLikes) {
+    throw new ApiError(400, "Error fetching total likes.");
+  }
+
+  return res.status(200).json(
+    new ApiResponse(
+      200,
+      {
+        totalVideoViews: totalVideoViews[0]?.viewsCount || 0,
+        totalSubscribers,
+        totalVideos,
+        totalLikes: totalLikes[0]?.totalNumberOfLikes || 0,
+      },
+      "Channel stats fetched successfully."
+    )
+  );
 });
+
+const getChannelVideos = asyncHandler(async (req, res) => {
+  const { channelId } = req.params;
+  if (!isValidObjectId(channelId)) {
+    throw new ApiError(400, "Invalid channel id.");
+  }
+
+  const videos = await Video.find({ owner: channelId }).sort({
+    createdAt: -1,
+  });
+
+  if (!videos) {
+    throw new ApiError(404, "No videos found for this channel.");
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, videos, "Channel videos fetched successfully"));
+});
+
+export { getChannelStats, getChannelVideos };
